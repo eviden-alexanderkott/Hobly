5903218c4e699eb97fccc2a1fc06a91a975f75fe
Alexander <alexander.kott@eviden.com>
2024-02-07 11:01:02 +0100

    ja

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,3 @@
-// contact.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,12 +9,29 @@ export class ContactComponent {
   name: string = '';
   email: string = '';
   message: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
 
   constructor() {}
 
   submitForm(): void {
-    // Hier können Sie die Logik für den Formularversand oder die Verarbeitung implementieren
+    if (!this.name || !this.email || !this.message) {
+      this.errorMessage = 'Bitte füllen Sie alle Felder aus.';
+      this.successMessage = ''; // Reset success message
+      return; // Don't proceed if any field is empty
+    }
+
+    // Hier könnten weitere Validierungen durchgeführt werden, bevor das Formular gesendet wird
+
+    // Hier könnte die Logik für den Formularversand oder die Verarbeitung implementiert werden
     console.log('Formular gesendet:', this.name, this.email, this.message);
+    
+    // Erfolgsmeldung setzen
+    this.successMessage = 'Vielen Dank für Ihre Nachricht! Wir werden uns bald bei Ihnen melden.';
+    
+    // Fehlermeldung zurücksetzen
+    this.errorMessage = '';
+
     // Setzen Sie die Formularfelder zurück
     this.name = '';
     this.email = '';
